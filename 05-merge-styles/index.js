4d065fd476bc9eee30a5f7516433fae6d350f483
TarasiukDima <dimatarasiuk91@gmail.com>
2021-10-29 19:35:51 +0300

    feat: end merge styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+const cssFolder = path.join(__dirname, 'styles');
+const outputFolder = path.join(__dirname, 'project-dist', 'bundle.css');
+
+
+const bundleCss = (inputF, outputF) => {
+  const newFile = fs.createWriteStream(outputF);
+
+  fs.access(inputF, (err) => {
+    if (err) throw err;
+
+    fsPromises.readdir(inputF, { withFileTypes: true })
+      .then(data => {
+        data.forEach(el => {
+          if (!el.isFile() || path.extname(el.name) !== '.css') return;
+
+          let content = fs.createReadStream(path.join(inputF, el.name), 'utf-8');
+          content.on('data', (text) => newFile.write(text));
+        });
+      })
+      .catch(err => console.log(err));
+  });
+};
+
+bundleCss(cssFolder, outputFolder);
