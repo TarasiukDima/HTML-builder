6cadfec75ef6075e93e715aefceb7b40b59b0924
TarasiukDima <dimatarasiuk91@gmail.com>
2021-10-29 15:17:24 +0300

    feat: end copy directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+const folderPath = path.join(__dirname, 'files');
+const newfolderPath = path.join(__dirname, 'files-copy');
+
+const copyDir = (folderName, newFilderName) => {
+  fs.access(newFilderName, (err) => {
+    if (err) {
+      fsPromises.mkdir(newFilderName, { recursive: true })
+        .catch(err => console.log(err));
+    }
+
+    fsPromises.readdir(folderName, { withFileTypes: true })
+      .then(data => {
+        data.forEach(el => {
+          fsPromises.copyFile(path.join(folderName, el.name), path.join(newFilderName, el.name));
+        });
+      })
+      .catch(err => console.log(err));
+  });
+};
+
+copyDir(folderPath, newfolderPath);
